91cf3a0a776361c0bbb600d78dc4d4df13c907ab
Mercy <mercyjesintha.arul@aspiresys.com>
2024-12-13 16:59:21 +0530

    html tags replacement

--- src/pages/chats-page/chats.tsx
+++ src/pages/chats-page/chats.tsx
@@ -5,33 +5,30 @@ interface IChatProps {
   chat: IChat;
 }
 
-export const Chats = ({ chat }: IChatProps) => {
-  const element = document.getElementById("response-string");
-  if (element) element.innerHTML = chat.chatGptResponse ?? "";
-
-  return (
-    <ConversationContainer>
-      {chat.chatType === 1 && (
-        <QueryContainer>
-          <div className="response">&nbsp;</div>
-          <div className="user-query">
-            <div className="user-query-label">
-              <p>{chat.question}</p>
-            </div>
-            <div className="user-query-rect"></div>
+export const Chats = ({ chat }: IChatProps) => (
+  <ConversationContainer>
+    {chat.chatType === 1 && (
+      <QueryContainer>
+        <div className="response">&nbsp;</div>
+        <div className="user-query">
+          <div className="user-query-label">
+            <p>{chat.question}</p>
           </div>
-        </QueryContainer>
-      )}
-      {chat.chatType === 2 && (
-        <QueryContainer>
-          <div className="response">
-            <div className="response-label">
-              <p id="response-string"></p>
-            </div>
-            <div className="response-rect"></div>
+          <div className="user-query-rect"></div>
+        </div>
+      </QueryContainer>
+    )}
+    {chat.chatType === 2 && (
+      <QueryContainer>
+        <div className="response">
+          <div className="response-label">
+            <p
+              dangerouslySetInnerHTML={{ __html: chat.chatGptResponse ?? "" }}
+            />
           </div>
-        </QueryContainer>
-      )}
-    </ConversationContainer>
-  );
-};
+          <div className="response-rect"></div>
+        </div>
+      </QueryContainer>
+    )}
+  </ConversationContainer>
+);
