a59fc0d31e98d80cf8ba51425e6b3e32943e624b
Mercy <mercyjesintha.arul@aspiresys.com>
2024-12-13 16:35:28 +0530

    html tags replacement

--- src/pages/chats-page/chats.tsx
+++ src/pages/chats-page/chats.tsx
@@ -5,39 +5,33 @@ interface IChatProps {
   chat: IChat;
 }
 
-const decodeHtmlEntities = (input: string): string => {
-  const doc = new DOMParser().parseFromString(input, "text/html");
-  return doc.documentElement.textContent || "";
-};
+export const Chats = ({ chat }: IChatProps) => {
+  const element = document.getElementById("response-string");
+  if (element) element.innerHTML = chat.chatGptResponse ?? "";
 
-export const Chats = ({ chat }: IChatProps) => (
-  <ConversationContainer>
-    {chat.chatType === 1 && (
-      <QueryContainer>
-        <div className="response">&nbsp;</div>
-        <div className="user-query">
-          <div className="user-query-label">
-            <p>{chat.question}</p>
+  return (
+    <ConversationContainer>
+      {chat.chatType === 1 && (
+        <QueryContainer>
+          <div className="response">&nbsp;</div>
+          <div className="user-query">
+            <div className="user-query-label">
+              <p>{chat.question}</p>
+            </div>
+            <div className="user-query-rect"></div>
           </div>
-          <div className="user-query-rect"></div>
-        </div>
-      </QueryContainer>
-    )}
-    {chat.chatType === 2 && (
-      <QueryContainer>
-        <div className="response">
-          <div className="response-label">
-            <p>
-              {decodeHtmlEntities(
-                chat.chatGptResponse
-                  ?.replace("<br />", "&#10;")
-                  .replace("<br>", "&#10;") ?? ""
-              )}
-            </p>
+        </QueryContainer>
+      )}
+      {chat.chatType === 2 && (
+        <QueryContainer>
+          <div className="response">
+            <div className="response-label">
+              <p id="response-string"></p>
+            </div>
+            <div className="response-rect"></div>
           </div>
-          <div className="response-rect"></div>
-        </div>
-      </QueryContainer>
-    )}
-  </ConversationContainer>
-);
+        </QueryContainer>
+      )}
+    </ConversationContainer>
+  );
+};
